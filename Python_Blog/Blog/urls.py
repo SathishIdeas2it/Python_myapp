a06e4d79a11104d5460c81fb6312f5bf142810a4
NTST\agilank87 <akuppusamy@ntst.com>
2023-05-24 23:01:10 +0530

    API integration and Logger

--- Python_Blog/Blog/urls.py
+++ Python_Blog/Blog/urls.py
@@ -20,5 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("accounts.urls")),
-    path("WebScraping",include("WebScraping.urls"))
+    path("WebScraping",include("WebScraping.urls")),
+    path("ConsumeAPI",include("ConsumeAPI.urls"))
 ]
--- Python_Blog/ConsumeAPI/views.py
+++ Python_Blog/ConsumeAPI/views.py
@@ -1,30 +1,30 @@
-from django.shortcuts import redirect, render
 from bs4 import BeautifulSoup
+from django.shortcuts import render
+from django.views import generic
+import config_reader.config as config
 import requests
 from django.http import HttpResponse, HttpResponseRedirect
-import config_reader.config as config
-import logging 
+import logging
 
 # Create your views here.
 
-def index(request):    
-              
+def consume_api(request):
     HEADERS = ({'User-Agent':config.read_config('Header','user_agent'),'Accept-Language':config.read_config('Header','accept_language')})    
     
     try:
            
         # Reading url from Config file(config.toml)
-        page = requests.get(config.read_config("API","scraping_url"),headers=HEADERS)
+        page = requests.get(config.read_config("API","nowrunning_movie_url"), headers=HEADERS)
 
-        soup =  BeautifulSoup(page.content, "html.parser")          
-        
-        links = soup.find_all('a',attrs={'class':'Title__title__z5HRm Title__fixed__bJ2c2'})  
+        soup =  BeautifulSoup(page.content, "html.parser")     
+           
+        links = soup.find_all('div',attrs={'class':'movie_s'}) 
         
         data = []
-            
+                            
         for link in links:
                                 
-            item_page = requests.get(config.read_config("API","amazon_url") + link.get('href') ,headers=HEADERS)         
+            '''item_page = requests.get(config.read_config("API","amazon_url") + link.get('href') ,headers=HEADERS)         
             item_soup =  BeautifulSoup(item_page.content, "html.parser")          
         
             title = item_soup.find('span',attrs={'id':'productTitle'}).text      
@@ -32,20 +32,25 @@ def index(request):
             image = item_soup.find('div',attrs={'id':'imgTagWrapperId'}).find('img').get('src')         
             offer_price = item_soup.find('span', attrs={'class':'a-price-whole'}).text.replace('.','')
             original_price = item_soup.find('span',attrs={'class':'a-price a-text-price'}).find('span', attrs={'class':'a-offscreen'}).text.replace('₹','').replace('.00','')
-            rating= item_soup.find('span', attrs={'class':'a-icon-alt'}).text          
-                
+            rating= item_soup.find('span', attrs={'class':'a-icon-alt'}).text '''       
+           
+            #breakpoint()
+             
+            title = link.find('a', attrs={'class':'list_title strong'}).text  
+            url = config.read_config("API","nowrunning_url") + link.find('a', attrs={'class':'list_title strong'}).get('href')  
+            img = link.find('amp-img').get('src')                     
+           
             value = {   
                 'title' : title,
+                'website' : "NowRunning",
                 'url' : url,
-                'image' : image,
-                'offer_price' : offer_price,
-                'original_price' : original_price,
-                'rating' : rating
+                'year' : '2023',
+                'image' : img
             }
             
             data.append(value)   
                         
-        return render(request,'WebScraping\scrap-data.html',{'data':data})
+        return render(request,'ConsumeAPI\movie-list.html',{'data':data})
     except Exception as e:      
         error_message = 'user:' + str(request.user) + ', error:'+ str(e)     
         logging.getLogger("error_logger").error(error_message)
